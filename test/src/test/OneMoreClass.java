bf1d995b9381b5f474b6698076701f88afb47791
elias2211 <32076201+elias2211@users.noreply.github.com>
2019-02-05 00:46:15 -0600

    Added one more test class

---
+++ test/src/test/OneMoreClass.java
@@ -0,0 +1,69 @@
+package test;
+
+public class OneMoreClass {
+	private String name = null;
+	private int age;
+	private String password = null;
+	
+	OneMoreClass(){
+	}
+	
+	public OneMoreClass(String name, int age, String password) {
+		super();
+		this.name = name;
+		this.age = age;
+		this.password = password;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the age
+	 */
+	public int getAge() {
+		return age;
+	}
+
+	/**
+	 * @param age the age to set
+	 */
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "name=" + name + ", age=" + age + ", password=" + password;
+	}
+	
+	
+}
--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -4,7 +4,12 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test");
+		OneMoreClass firstPerson = new OneMoreClass();
+			firstPerson.setName("Elias");
+			firstPerson.setAge(32);
+			firstPerson.setPassword("password");
+				System.out.println(firstPerson.toString());
+			
 	}
 
 }
